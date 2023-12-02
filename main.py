d982a296c48992938e06017394199a8ee28025d4
tmukker <tusharmukker7@gmail.com>
2023-12-01 19:51:26 -0500

    Final commit

--- main.py
+++ main.py
@@ -1,109 +1,182 @@
-import json
 import redis
-from pymongo import MongoClient
-from urllib.parse import quote_plus
+from pymongo.server_api import ServerApi
 import logging
+from pymongo import MongoClient, errors
+import json
+import numpy as np
+import time
+import statistics
+import csv
 
-class MovieQueryFacade:
-    def __init__(self, redis_host, redis_port, redis_password, mongo_uri, db_name="sample_mflix"):
-        self.redis_host = redis_host
-        self.redis_port = redis_port
-        self.redis_password = redis_password
-        self.mongo_uri = mongo_uri
-        self.db_name = db_name
-
-        self.redis_client = None
-        self.mongo_client = None
-        self.db = None
-
-        self._connect_to_redis()
-        self._connect_to_mongo()
-
-    def _connect_to_redis(self):
+if __name__ == '__main__':
+    # =========================== Reading from Redis ===========================
+    def readFromRedis(key):
         try:
-            # Connect to Redis
-            self.redis_client = redis.StrictRedis(
-                host=self.redis_host, port=self.redis_port, password=self.redis_password, decode_responses=True
-            )
-            logging.info("Connected to Redis")
-        except Exception as e:
-            logging.error(f"Error connecting to Redis: {e}")
-
-    def _connect_to_mongo(self):
-        try:
-            # Connect to MongoDB with username and password
-            mongo_uri_with_auth = self.mongo_uri.format(
-                username=quote_plus(""),  # replace with your MongoDB username
-                password=quote_plus("")   # replace with your MongoDB password
-            )
-            self.mongo_client = MongoClient(mongo_uri_with_auth)
-            self.db = self.mongo_client[self.db_name]
-            logging.info(f"Connected to MongoDB. Using database: {self.db_name}")
-        except Exception as e:
-            logging.error(f"Error connecting to MongoDB: {e}")
-
-    def query_top_n(self):
-        try:
-            top_n = int(input("Enter the value for top_n: "))
-            from_year = int(input("Enter the starting year: "))
-            to_year = int(input("Enter the ending year: "))
-        except ValueError:
-            logging.error("Invalid input. Please enter valid integers.")
-            return None
-
-        if self.db is None:
-            logging.error("MongoDB connection not established.")
-            return None
-
-        try:
-            # Check Redis for existing data
-            redis_key = f"top-{top_n}-{from_year}-{to_year}"
-            redis_data = self.redis_client.get(redis_key)
-
-            if redis_data:
-                # If data exists in Redis, return it
-                return redis_data
+            value = redis_client.get(key)
+            if value:
+                return json.loads(value.decode('utf-8'))
             else:
-                # Data doesn't exist in Redis, develop MongoDB aggregation pipeline
+                return None
+        except Exception as err_redis:
+            logging.error("Error reading from Redis: {}".format(err_redis))
+
+    # =========================== Writing to Redis ===========================
+    def writeToRedis(key, value):
+        try:
+            redis_client.set(key, json.dumps(value))
+        except Exception as err_redis:
+            logging.error("Error writing to Redis: {}".format(err_redis))
+
+    # =========================== Writing to MongoDB ===========================
+    def writeToMongoDB(key, value, unique_field='title'):
+        try:
+            with MongoClient(mongo_uri, tls=True, tlsCertificateKeyFile='YOUR_CERTIFICATE.pem',
+                             server_api=ServerApi('1')) as mongodb_client:
+                mongodb_db = mongodb_client['sample_mflix']
+
+                if key in mongodb_db.list_collection_names():
+                    for document in value:
+                        identifier = document.get(unique_field, 'default_identifier')
+                        filter_criteria = {unique_field: identifier}
+                        update_data = {'$set': document}
+                        mongodb_db[key].update_one(filter_criteria, update_data, upsert=True)
+                else:
+                    mongodb_collection = mongodb_db[key]
+                    if isinstance(value, str):
+                        value = json.loads(value)
+                    mongodb_collection.insert_many(value)
+        except errors.PyMongoError as err_mongo:
+            logging.error(f"Error writing to MongoDB for key {key}: {err_mongo}")
+
+    # =========================== Cleaning JSON String ===========================
+    def clean_json(data):
+        data = data.replace("'", '"')
+        data = data.replace("\\", "")
+        return data
+
+    # =========================== Query ===========================
+    def query_top_n(n, from_year, to_year):
+        try:
+            start_time = time.time()
+            key = f"top-{n}-{from_year}-{to_year}"
+            value = readFromRedis(key)
+
+            if value:
+                print("Querying Redis")
+                logging.info("Querying Redis")
+                writeToMongoDB(key, value)
+                result = value
+            else:
+                print("Querying MongoDB")
+                logging.info("Querying MongoDB")
                 pipeline = [
                     {"$match": {"year": {"$gte": from_year, "$lte": to_year}}},
-                    {"$unwind": "$comments"},
-                    {"$group": {
-                        "_id": "$_id",
-                        "title": {"$first": "$title"},
-                        "comment_count": {"$sum": 1}  # Counting comments
-                    }},
+                    {"$group": {"_id": {"title": "$title", "year": "$year"}, "comment_count": {"$sum": 1}}},
+                    {"$project": {"_id": 0, "title": "$_id.title", "year": "$_id.year", "comment_count": 1}},
                     {"$sort": {"comment_count": -1}},
-                    {"$limit": top_n},
-                    {"$project": {"_id": 0, "name": "$title", "comment_count": 1}}
+                    {"$limit": n}
                 ]
+                cursor = mongo_db.movies.aggregate(pipeline)
+                result = json.loads(json.dumps(list(cursor)))
+                writeToRedis(key, result)
 
-                # Execute aggregation pipeline in MongoDB
-                result_from_mongo = list(self.db.movies.aggregate(pipeline))
+            end_time = time.time()
+            execution_time = end_time - start_time
+            print(f"Query Execution Time: {execution_time} seconds")
+            return result, execution_time
+        except Exception as err:
+            logging.error("Error querying MongoDB: {}".format(err))
 
-                # Convert result to JSON format
-                json_result = json.dumps(result_from_mongo, default=str)
+    # =========================== Test Case 1 - Empty Redis ===========================
+    def performance_test_case_1(query_function, n, from_year, to_year, trials):
+        execution_times = []
 
-                # Save result to Redis for future retrieval
-                self.redis_client.set(redis_key, json_result)
+        for _ in range(trials):
+            redis_client.flushall()  # Clear Redis
+            result, execution_time = query_function(n, from_year, to_year)
+            execution_times.append(execution_time)
 
-                # Save result to MongoDB collection with a dynamic name
-                collection_name = f"top-{top_n}-{from_year}-{to_year}"
-                self.db[collection_name].insert_many(result_from_mongo)
-            return json_result
-        except Exception as e:
-            logging.error(f"Error querying MongoDB: {e}")
-            return None
+        avg_time = statistics.mean(execution_times)
+        percentile_50 = statistics.median(execution_times)
 
-    # Configure logging
-logging.basicConfig(level=logging.INFO)
-# Example usage:
-redis_host = ''
-redis_port = 12719  # Default Redis port
-redis_password = ''
-mongo_uri = 'mongodb+srv://{username}:{password}@cluster.64elgvb.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+        if len(execution_times) > 0:
+            percentile_90 = np.percentile(execution_times, 90)
+        else:
+            percentile_90 = 0  # Set a default value if the list is empty
 
-facade = MovieQueryFacade(redis_host, redis_port, redis_password, mongo_uri)
-result = facade.query_top_n()
+        return avg_time, percentile_50, percentile_90, execution_times
 
-print(result)
+    # =========================== Test Case 2 - Redis with Data ===========================
+    def performance_test_case_2(query_function, n, from_year, to_year, trials):
+        execution_times = []
+
+        for _ in range(trials):
+            result, execution_time = query_function(n, from_year, to_year)
+            execution_times.append(execution_time)
+
+        avg_time = statistics.mean(execution_times)
+        percentile_50 = statistics.median(execution_times)
+
+        if len(execution_times) > 0:
+            percentile_90 = np.percentile(execution_times, 90)
+        else:
+            percentile_90 = 0
+
+        return avg_time, percentile_50, percentile_90, execution_times
+
+    print("Running App.....\n")
+
+    # =========================== Connecting to MongoDB ===========================
+    try:
+        mongo_uri = ("YOUR_MONGODB_URI")
+        mongo_client = MongoClient(mongo_uri, tls=True, tlsCertificateKeyFile='YOUR_CERTIFICATE.pem', server_api=ServerApi('1'))
+        mongo_db = mongo_client['sample_mflix']
+        logging.info('Connected to MongoDB')
+    except Exception as e:
+        logging.error(f"Error connecting to MongoDB: {e}")
+
+    # =========================== Connecting to Redis ===========================
+    try:
+        redis_host = 'YOUR_REDIS_HOST'
+        redis_port = "YOUR_REDIS_PORT"
+        redis_password = 'YOUR_REDIS_PASSWORD'
+
+        if redis_password:
+            redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
+            logging.info("Connected to Redis")
+        else:
+            logging.error("Supply a password for Redis")
+    except Exception as e:
+        logging.error("Error connecting to Redis: {}".format(e))
+
+    # Get inputs from user
+    top_n = int(input("Enter the Number top n: "))
+    t_from = int(input("From: "))
+    t_to = int(input("To: "))
+
+    # =========================== Performance Testing ===========================
+    num_trials = 100
+
+    # Case 1: Empty Redis
+    avg_time_empty, percentile_50_empty, percentile_90_empty, execution_times_empty = performance_test_case_1(
+        query_top_n, top_n, t_from, t_to, num_trials)
+
+    # Case 2: Redis with data
+    avg_time_with_data, percentile_50_with_data, percentile_90_with_data, execution_times_with_data = performance_test_case_2(
+        query_top_n, top_n, t_from, t_to, num_trials)
+
+    # Save results to CSV
+    csv_headers = ["Case", "Average Time", "50th Percentile", "90th Percentile", "Execution Times"]
+    csv_data = [
+        ["Empty Redis", avg_time_empty, percentile_50_empty, percentile_90_empty, execution_times_empty],
+        ["Redis with Data", avg_time_with_data, percentile_50_with_data, percentile_90_with_data,
+         execution_times_with_data],
+    ]
+
+    csv_file_path = "performance_results.csv"
+    with open(csv_file_path, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(csv_headers)
+        writer.writerows(csv_data)
+
+    print(f"Performance results saved to {csv_file_path}")
